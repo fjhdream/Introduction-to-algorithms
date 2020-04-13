60e127a14c6c0276a3286a012fca3e9218378652
fjhdream <fjh_dream@163.com>
2020-04-13 20:45:51 +0800

    ch2 achived by go

---
+++ Ch2-Sort/insertion-sort.go
@@ -0,0 +1,33 @@
+package main
+
+func insertionSort(array []int) []int {
+	for i := 1; i < len(array); i++ {
+		key := array[i]
+		j := i - 1
+		for j >= 0 && array[j] > key {
+			//fmt.Println("key:", key, "arry[j]:", array[j])
+			array[j], array[j+1] = array[j+1], array[j]
+			j = j - 1
+			//fmt.Println(array)
+		}
+		array[j+1] = key
+	}
+
+	return array
+}
+
+// func main() {
+// 	exampleArray := []int{2, 5, 4, 3, 1}
+
+// 	timeStart := time.Now()
+// 	_ = insertionSort(exampleArray)
+// 	timeRunning := time.Since(timeStart)
+// 	fmt.Printf("the length of %d array sorts cost %d ns\n", len(exampleArray), timeRunning)
+
+// 	rand.Seed(time.Now().UnixNano())
+// 	exampleArray = rand.Perm(1000)
+// 	timeStart = time.Now()
+// 	_ = insertionSort(exampleArray)
+// 	timeRunning = time.Since(timeStart)
+// 	fmt.Printf("the length of %d array sorts cost %d ns\n", len(exampleArray), timeRunning)
+// }
---
+++ Ch2-Sort/merge-sort.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func merge(array []int, p, q, r int) {
+	intMax := int(^uint(0) >> 1)
+	leftArr := append([]int{}, array[p:q]...)
+	rightArr := append([]int{}, array[q:r]...)
+	leftArr = append(leftArr, intMax)
+	rightArr = append(rightArr, intMax)
+
+	i := 0
+	j := 0
+	for k := p; k < r; k++ {
+		if leftArr[i] <= rightArr[j] {
+			array[k] = leftArr[i]
+			i = i + 1
+		} else {
+			array[k] = rightArr[j]
+			j = j + 1
+		}
+	}
+
+}
+
+func mergeSort(array []int, p, r int) {
+	q := int(math.Floor(float64((p + r) / 2)))
+	if p < r && q < r && p < q {
+		go mergeSort(array, p, q)
+		go mergeSort(array, q, r)
+		go merge(array, p, q, r)
+	} else {
+		return
+	}
+
+}
+
+func main() {
+	timeStart := time.Now()
+	array := rand.Perm(1000)
+	mergeSort(array, 0, len(array))
+	timeRunnig := time.Since(timeStart)
+	fmt.Printf("time cost: %d ns \n", timeRunnig)
+}
